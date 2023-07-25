ec3e8d2e9a0188d7dd4b72a2107db1606884e513
Viktoria Konovchenko <novoselova_viktoria146@mail.ru>
2023-07-25 19:18:03 +0300

    seminar_2

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// int number = new Random().Next(1,8);
+// Console.WriteLine(number);
+
+// if (number ==1)
+// {
+//     Console.WriteLine("Нет");
+// }
+// if (number ==2)
+// {
+//     Console.WriteLine("Нет");
+// }
+// if (number ==3)
+// {
+//     Console.WriteLine("Нет");
+// }
+// if (number ==4)
+// {
+//     Console.WriteLine("Нет");
+// }
+// if (number ==5)
+// {
+//     Console.WriteLine("Нет");
+// }
+// if (number ==6)
+// {
+//     Console.WriteLine("Да");
+// }
+// if (number ==7)
+// {
+//     Console.WriteLine("Да");
+// }
+
+// int number = new Random().Next(100,1000);
+// Console.WriteLine(number);
+
+// string numberText = Convert.ToString(number);
+// if (numberText.Length > 2)
+// {
+//     Console.WriteLine(numberText[2]);
+// }
+
+// int number = new Random().Next(100,1000);
+// Console.WriteLine(number);
+
+// string numberText = Convert.ToString(number);
+// if (numberText.Length > 1)
+// {
+//     Console.WriteLine(numberText[1]);
+// }
